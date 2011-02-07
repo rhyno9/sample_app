da0c65f92bf99ed0b3fcd848ff0c909ab0869c0f
Ryan K. Vander Zanden <ryan_vanderzanden@yahoo.com>
2011-02-07 10:08:18 -0600

    Done with user edit/update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,6 +53,25 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
+  describe "admin attribute" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 
  describe "password validations" do
     before(:each) do
